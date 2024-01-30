985e4c53a37a803e9e7f3e07972317239949ead3
Vasanth R <112184729+VasanthRznt@users.noreply.github.com>
2024-01-30 13:39:02 +0530

    Add files via upload

--- content.js
+++ content.js
@@ -1,18 +1,8 @@
 // content.js
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   if (request.action === 'scrapeContent') {
-    // Replace 'your-selector' with the appropriate CSS selector for your specific content
-    const specificElement = document.querySelectorAll("div.a-expander-content.reviewText.review-text-content.a-expander-partial-collapse-content span");
-    const r=[];
-    for (let i = 0; i < specificElement.length; i++) {     
-       const name = specificElement[i]?.innerText.trim() || 'N/A';r.push(name)}
-console.log(specificElement);
-    if (specificElement) {
-      // Extract text content or other data from the selected element
-      const scrapedContent = specificElement.innerText;
-      sendResponse({ content: r });
-    } else {
-      sendResponse({ error: 'Element not found' });
-    }
+    // Implement your scraping logic here
+    const scrapedContent = document.body.innerText;
+    sendResponse({content: scrapedContent});
   }
 });
