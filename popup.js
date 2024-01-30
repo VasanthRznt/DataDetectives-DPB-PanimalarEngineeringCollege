9ca63264fa9ae51eedd020cde3d039988154b494
Vasanth R <112184729+VasanthRznt@users.noreply.github.com>
2024-01-30 13:37:55 +0530

    Add files via upload

---
+++ popup.js
@@ -0,0 +1,8 @@
+// popup.js
+document.getElementById('scrapeButton').addEventListener('click', function() {
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, {action: 'scrapeContent'}, function(response) {
+      document.getElementById('result').innerText = response.content;
+    });
+  });
+});
